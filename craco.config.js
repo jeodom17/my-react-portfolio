8bea9509e5f0bba4cb9e0fdce352a59b6bbbd9eb
jeodom17 <jeodom17@gmail.com>
2021-11-16 17:46:35 -0500

    add components folder/files

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+//* craco configuration
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }
