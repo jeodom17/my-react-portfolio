bd5f3ab0f32b7d85ea4da521032056d98c742774
jeodom17 <jeodom17@gmail.com>
2021-11-16 23:14:35 -0500

    adding about me and project info

--- src/components/Project.js
+++ src/components/Project.js
@@ -43,32 +43,33 @@ export default function Project() {
           <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
   <div class="md:flex">
     <div class="lg:flex-shrink-0">
-      <img class="h-60 w-full object-cover md:w-60" src={require("..//assets/img/makko.jpg").default} alt="Makko"></img>
+      <img class="h-60 w-full object-cover md:w-60" src={require("..//assets/img/profilepic.png").default} alt="Profile Pic"></img>
     </div>
     <div class="p-12">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-      <a href="google.com" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-      <p class="mt-2 text-emerald-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+      <div class="uppercase tracking-wide text-lg text-indigo-500 font-semibold">Justin E. Odom</div>
+      <p class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Let's Build Something!</p>
+      <p class="mt-2 text-emerald-500">I am a Junior full stack web developer seeking a position in web development. Experienced business Project Manager / Executive with 14 years of experience working with customers and employees to provide services in the industrial and commercial markets. Experience includes working with customers, managing projects and employees and workforce development.</p>
     </div>
   </div>
 </div>
 <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-  <section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">  
+  <section class="max-w-6xl mx-auto px-2 sm:px-3 lg:px-6 py-12">  
     <div class="text-center pb-12">
         <h2 class="text-base font-bold text-indigo-600">
             We have the best equipment in the market
         </h2>
         <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
-            Check our awesome team members
+            Check out some of my Projects
         </h1>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
             <div>
-                <img class="object-center object-cover h-auto w-full" src={require("..//assets/img/makko.jpg").default} alt="Makko"></img>
+                <h1>Reloc8</h1>
+                <img class="object-center object-cover h-auto w-full" src={require("..//assets/img/reloc8.gif").default} alt="Reloc8"></img>
             </div>
             <div class="text-center py-8 sm:py-6">
-                <p class="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
+                <p class="text-xl text-gray-700 font-bold mb-2">Reloc8</p>
                 <p class="text-base text-gray-400 font-normal">Software Engineer</p>
             </div>
         </div>
